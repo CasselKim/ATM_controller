868c6bec55837e394793da35fa8b9a7eabf731fd
CasselKim <seongr39@gmail.com>
2022-03-26 00:14:56 +0900

    update docstring

---
+++ ATM_Controller/atmIO.py
@@ -0,0 +1,16 @@
+'''
+atmIO.py
+===========
+this module is for communication with the ATM.
+
+Methods:
+    read_Card() -> str
+    check_valid_input(PIN : uuid4) -> bool
+    display_accounts(accounts : List[str]) -> bool
+    get_selected_account() -> str
+    display_account_info(accounts : Dict[str, int]) -> bool
+
+Todo: 
+    errors
+      - custom errors for this project
+'''
